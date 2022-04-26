33e86824ba0b35d704c66ef159d118673057000c
ZeranXu <zeranxu@usc.edu>
2022-04-25 18:10:40 -0700

    Create first.py

---
+++ hellopython/first.py
@@ -0,0 +1,45 @@
+
+#printing hello world
+#this is how you print statements
+
+print("Hello world")
+
+
+# create a variable called iris and give it a value
+iris="macbook"
+
+# print that value
+print(iris)
+
+# print the string iris
+# print ("iris")
+
+
+
+# data types
+
+# strings
+print ("hello")
+print ("1")
+
+# integers
+print (1)
+
+# floats
+print(2.5)
+
+# lists - collection of different data types
+
+fruits=["apple", "orange", "kiwi"]
+
+print(fruits)
+
+#dictionary - collection of key value pairs
+
+students={"eileen":"chen", "josh":"code"}
+
+print(students)
+
+#python 2
+#print "hello world"
+
